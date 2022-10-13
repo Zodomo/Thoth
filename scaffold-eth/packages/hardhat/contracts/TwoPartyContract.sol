cfff4062bfbc0a0588b071dcdb8108f1d46434d6
Zodomo <zodomo@proton.me>
2022-10-12 23:33:08 -0500

    fixed auto-verify, revamped function calls for hash-only input, redid getContractData()

--- scaffold-eth/packages/hardhat/contracts/TwoPartyContract.sol
+++ scaffold-eth/packages/hardhat/contracts/TwoPartyContract.sol
@@ -19,9 +19,20 @@ contract TwoPartyContract {
   mapping(bytes32 => bool) public contractExecuted; // Contract Hash => True/False, contract automatically executes when both parties sign
   mapping(bytes32 => bytes[]) public contractSignatures; // Contract Hash => Signatures
 
+  // Contract struct required for getContractData() function as Solidity can't handle returning 7 variables. Returning a struct is technically only one
+  struct Contract {
+    address initiator;
+    address counterParty;
+    string ipfsHash;
+    uint256 blockNumber;
+    bool executed;
+    bytes firstSignature;
+    bytes secondSignature;
+  }
+
   // Log contract initiator address, counterParty address, ipfsHash/Pointer string, and blockNumber agreement is in
   // counterParty is the only unindexed parameter because EVM only allows for three and I found counterParty to be the least relevant
-  event ContractCreated(address indexed initiator, address counterParty, string indexed ipfsHash, uint256 indexed blockNumber);
+  event ContractCreated(address initiator, address counterParty, string indexed ipfsHash, uint256 indexed blockNumber, bytes32 indexed contractHash);
   // Log contract hashes on their own as all contrct details in ContractCreated can be obtianed by querying granular contract data mappings (contractParties, ...)
   event ContractHashed(bytes32 indexed contractHash);
   // Log contract signatures, contractHash used in verification, and the signer address to validate against
@@ -47,8 +58,13 @@ contract TwoPartyContract {
   }
 
   // Require contract creation by checking if _party1 is part of a contract with _party2
-  modifier validParty(address _counterParty, string memory _ipfsHash, uint256 _blockNum) {
-    require(bytes32(contractHashes[msg.sender][_counterParty][_ipfsHash][_blockNum]).length > 0, "Contract not created");
+  modifier validParty(bytes32 _contractHash) {
+    require(contractParties[_contractHash][0] == msg.sender || contractParties[_contractHash][1] == msg.sender);
+    _;
+  }
+
+  modifier notExecuted(bytes32 _contractHash) {
+    require(!contractExecuted[_contractHash], "Contract already executed");
     _;
   }
 
@@ -66,6 +82,12 @@ contract TwoPartyContract {
   // Hash of: Party1 Address + Party2 Address + IPFS Hash + Block Number Agreement Proposed In
   function getMessageHash(address _party1, address _party2, string memory _ipfsHash, uint256 _blockNum) internal pure returns (bytes32) {
     return keccak256(abi.encodePacked(_party1, _party2, _ipfsHash, _blockNum));
+  }
+
+  // Ethereum signed message has following format:
+  // "\x19Ethereum Signed Message\n" + len(msg) + msg
+  function getEthSignedMessageHash(bytes32 _messageHash) internal pure returns (bytes32) {
+    return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", _messageHash));
   }
 
   // Split signature into (r, s, v) components so ecrecover() can determine signer
@@ -87,16 +109,9 @@ contract TwoPartyContract {
     return ecrecover(_ethSignedMessageHash, v, r, s); // Recovers original signer from _ethSignedMessageHash and post-split _signature
   }
 
-  // Ethereum signed message has following format:
-  // "\x19Ethereum Signed Message\n" + len(msg) + msg
-  function getEthSignedMessageHash(bytes32 _messageHash) internal pure returns (bytes32) {
-    return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", _messageHash));
-  }
-
   // Verify if signature was for messageHash and that the signer is valid, public because interface might want to use this
-  function verifySignature(address _signer, address _counterParty, string memory _ipfsHash, uint256 _blockNum, bytes memory _signature) public view returns (bool) {
-    bytes32 messageHash = contractHashes[_signer][_counterParty][_ipfsHash][_blockNum];
-    bytes32 ethSignedMessageHash = getEthSignedMessageHash(messageHash);
+  function verifySignature(address _signer, bytes32 _contractHash, bytes memory _signature) public pure returns (bool) {
+    bytes32 ethSignedMessageHash = getEthSignedMessageHash(_contractHash);
     return recoverSigner(ethSignedMessageHash, _signature) == _signer;
   }
 
@@ -106,41 +121,43 @@ contract TwoPartyContract {
      The contract hash is what each party needs to sign */
   function hashContract(address _counterParty, string memory _ipfsHash, uint256 _blockNum) internal onlyInitiator(_counterParty, _ipfsHash, _blockNum) returns (bytes32) {
     bytes32 contractHash = getMessageHash(msg.sender, _counterParty, _ipfsHash, _blockNum);
+    // Save same contract hash for both parties. Initiator must be only caller as changing the address order changes the hash
     contractHashes[msg.sender][_counterParty][_ipfsHash][_blockNum] = contractHash;
     contractHashes[_counterParty][msg.sender][_ipfsHash][_blockNum] = contractHash;
+    // Save contract party addresses
     contractParties[contractHash].push(msg.sender);
     contractParties[contractHash].push(_counterParty);
+    // Save contract IPFS hash/pointer
     contractIpfsHash[contractHash] = _ipfsHash;
+    // Save block number agreement proposed in
     contractBlock[contractHash] = _blockNum;
     emit ContractHashed(contractHash);
     return contractHash;
   }
 
   // Instantiate two party contract with (msg.sender, counterparty address, IPFS hash of the contract document, current block number) and hash it, return block number of agreement proposal
-  function createTwoPartyContract(address _counterParty, string memory _ipfsHash) public returns (uint256) {
+  function createTwoPartyContract(address _counterParty, string memory _ipfsHash) public returns (bytes32) {
     require(bytes32(contractHashes[msg.sender][_counterParty][_ipfsHash][block.number]) == 0, "Contract already initiated in this block");
     // Need to instantiate hash field for each party to pass onlyInitiator check in hashContract()
     contractHashes[msg.sender][_counterParty][_ipfsHash][block.number] = bytes32("1");
-    hashContract(_counterParty, _ipfsHash, block.number);
-    emit ContractCreated(msg.sender, _counterParty, _ipfsHash, block.number);
-    return block.number;
+    bytes32 contractHash = hashContract(_counterParty, _ipfsHash, block.number);
+    emit ContractCreated(msg.sender, _counterParty, _ipfsHash, block.number, contractHash);
+    return contractHash;
   }
 
   // Commit signature to blockchain storage after verifying it is correct and that msg.sender hasn't already called signContract()
   // Consider cleaning function by migrating checks into modifiers
-  function signContract(address _counterParty, string memory _ipfsHash, uint256 _blockNum, bytes memory _signature) public validParty(_counterParty, _ipfsHash, _blockNum) {
-    bytes32 messageHash = contractHashes[msg.sender][_counterParty][_ipfsHash][_blockNum];
-    require(!contractExecuted[messageHash], "Contract already executed"); // Check if both parties have already signed
-    require(verifySignature(msg.sender, _counterParty, _ipfsHash, _blockNum, _signature), "Signature not valid");
-    if (contractSignatures[messageHash].length == 0) { // Push signature if no other signatures are stored
-      contractSignatures[messageHash].push(_signature);
-      emit ContractSigned(msg.sender, messageHash, _signature);
-    } else if (contractSignatures[messageHash].length == 1) { // Push signature if other party has signed and isn't trying to sign again
-      require(recoverSigner(getEthSignedMessageHash(messageHash), contractSignatures[messageHash][0]) != msg.sender, "Already signed");
-      contractSignatures[messageHash].push(_signature);
-      emit ContractSigned(msg.sender, messageHash, _signature);
-      contractExecuted[messageHash] = true;
-      emit ContractExecuted(messageHash, block.number);
+  function signContract(bytes32 _contractHash, bytes memory _signature) public validParty(_contractHash) notExecuted(_contractHash) {
+    require(verifySignature(msg.sender, _contractHash, _signature), "Signature not valid");
+    if (contractSignatures[_contractHash].length == 0) { // Push signature if no other signatures are stored
+      contractSignatures[_contractHash].push(_signature);
+      emit ContractSigned(msg.sender, _contractHash, _signature);
+    } else if (contractSignatures[_contractHash].length == 1) { // Push signature if other party has signed and isn't trying to sign again
+      require(recoverSigner(getEthSignedMessageHash(_contractHash), contractSignatures[_contractHash][0]) != msg.sender, "Already signed");
+      contractSignatures[_contractHash].push(_signature);
+      emit ContractSigned(msg.sender, _contractHash, _signature);
+      contractExecuted[_contractHash] = true;
+      emit ContractExecuted(_contractHash, block.number);
     } else { // Shouldn't ever be hit but will leave anyways
       revert("Two signatures already collected");
     }
@@ -151,28 +168,30 @@ contract TwoPartyContract {
   function verifyExecution(bytes32 _contractHash) public view hasExecuted(_contractHash) returns (bool) {
     bool party1;
     bool party2;
+    // Validate signatures against both addresses one at a time
     for (uint i = 0; i < contractSignatures[_contractHash].length; i++) {
-      if (verifySignature(contractParties[_contractHash][0], contractParties[_contractHash][1], contractIpfsHash[_contractHash], contractBlock[_contractHash], contractSignatures[_contractHash][i])) {
+      if (verifySignature(contractParties[_contractHash][0], _contractHash, contractSignatures[_contractHash][i])) {
         party1 = true;
       }
-      if (verifySignature(contractParties[_contractHash][1], contractParties[_contractHash][0], contractIpfsHash[_contractHash], contractBlock[_contractHash], contractSignatures[_contractHash][i])) {
+      if (verifySignature(contractParties[_contractHash][1], _contractHash, contractSignatures[_contractHash][i])) {
         party2 = true;
       }
     }
     return (party1 == party2);
   }
 
-  // Return all contract data using just the _contractHash
+  // Return all contract data using just the _contractHash, returning struct composition required due to stack limitations
   // Might be useful for frontend
-  function getContractData(bytes32 _contractHash) public view returns (address, address, string memory, uint256, bool, bytes memory, bytes memory) {
-    address initiator = contractParties[_contractHash][0];
-    address counterParty = contractParties[_contractHash][1];
-    string memory ipfsHash = contractIpfsHash[_contractHash];
-    uint256 blockNumber = contractBlock[_contractHash];
-    bool executed = contractExecuted[_contractHash];
-    bytes memory firstSignature = contractSignatures[_contractHash][0];
-    bytes memory secondSignature = contractSignatures[_contractHash][1];
-    return (initiator, counterParty, ipfsHash, blockNumber, executed, firstSignature, secondSignature);
+  function getContractData(bytes32 _contractHash) public view returns (Contract memory) {
+    Contract memory contractData;
+    contractData.initiator = contractParties[_contractHash][0];
+    contractData.counterParty = contractParties[_contractHash][1];
+    contractData.ipfsHash = contractIpfsHash[_contractHash];
+    contractData.blockNumber = contractBlock[_contractHash];
+    contractData.executed = contractExecuted[_contractHash];
+    contractData.firstSignature = contractSignatures[_contractHash][0];
+    contractData.secondSignature = contractSignatures[_contractHash][1];
+    return (contractData);
   }
 
   // Payment handling functions if we need them, otherwise just accept and allow withdrawal to any owner
